1c1c900160b24ebf609c38d80084e4028538881f
urbanodev81 <ricardo@focusmedia.com.br>
2024-04-08 17:12:16 -0300

    fontes personalizadas e uso do activityindicator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold, Roboto_700Bold_Italic } from '@expo-google-fonts/roboto';
+
 import theme from './src/theme'; 
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
